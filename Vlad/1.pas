4bb2bdfba01a760449068a59d21f5ddfcc77461d
fvi-games <105735556+fvi-games@users.noreply.github.com>
2022-10-06 22:09:00 +0300

    pas_1

---
+++ Vlad/1.pas
@@ -0,0 +1,11 @@
+program ZADACHA_PRACTICUM_1;
+uses crt;
+var r, a:byte; l, s:real;
+begin
+write ('r='); read(r);
+write ('a='); read(a);
+l:=r*a;
+s:=1/2*l*r;
+writeln ('l=', l);
+writeln ('s=', s:0:1);
+end.
